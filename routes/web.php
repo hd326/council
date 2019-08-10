011295d31984e3da31b36b00bdff44db5f16086c
hd326 <31417369+hd326@users.noreply.github.com>
2019-08-10 08:00:10 -0700

    Test update route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/threads', 'ThreadController@index')->name('threads');
-Route::get('/threads/create', 'ThreadController@create');
+Route::get('/threads/create', 'ThreadController@create')->middleware('must-be-confirmed');
 Route::get('/threads/{channel}/{thread}', 'ThreadController@show');
 Route::get('/threads/search', 'SearchController@show');
 
